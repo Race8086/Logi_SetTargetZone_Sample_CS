0d2d1a0bf30d675f10732be20ffd79b573ddfc75
Race8086 <rmsfree@gmail.com>
2022-07-15 17:59:53 +0200

    Git Init

--- Logi_SetTargetZone_Sample_CS/Program.cs
+++ Logi_SetTargetZone_Sample_CS/Program.cs
@@ -7,6 +7,13 @@ using LedCSharp;
 
 namespace Logi_SetTargetZone_Sample_CS
 {
+
+    /**
+     * 15/07/2022 Programa para porbar el api de logitech con el teclado G213
+     * Se ha utilizado el ejemplo suministrado por el fabricante, eliminando el código
+     * no referido al dispositivo
+     * 
+     * */
     public class Program
     {
         static void Main(string[] args)
@@ -26,19 +33,12 @@ namespace Logi_SetTargetZone_Sample_CS
             Console.WriteLine("Press \"ENTER\" to continue...");
             Console.ReadLine();
             LogitechGSDK.LogiLedSetTargetDevice(LogitechGSDK.LOGI_DEVICETYPE_ALL);
-
+            //La línea anterior inicializa cualquier disposito Logitech incluido en el API y conectado a la máquina.
             // Set all devices to Black
-            LogitechGSDK.LogiLedSetLighting(0, 0, 0);
-
-            // Set some keys on keyboard
-            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.L, 0, 100, 100);
-            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.O, 0, 100, 100);
-            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.G, 0, 100, 100);
-            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(keyboardNames.I, 0, 100, 100);
-            
-            // Set RGB mouse logo to Red
-            LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Mouse, 1, 100, 0, 0);
-
+            LogitechGSDK.LogiLedSetLighting(0, 0, 0); // Aunque la función de inicialización apaga las zonas se utiliza
+            // este comnando de nuevo.
+            // ** prueba de la función de Zona ( la zona 0 es todo el teclado
+            // G213 tiene 5 zonas adicionales, no se pueden direccionar teclas individuales
             // Set G213 keyboard zones to Red, Yellow, Green, Cyan, Blue
             LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Keyboard, 1, 100, 100, 100);
             LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Keyboard, 2, 100, 100, 100);
@@ -46,9 +46,9 @@ namespace Logi_SetTargetZone_Sample_CS
             LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Keyboard, 4, 100, 0, 0);
             LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Keyboard, 5, 0, 100, 0);
 
-            // Set G633/G933 headset logos to White, backsides to Purple
-            LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Headset, 0, 100, 100, 100);
-            LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Headset, 1, 100, 0, 100);
+            // Test Efects on G213 keyboard
+
+            
 
             Console.WriteLine("Press \"ENTER\" to exit...");
             Console.ReadLine();
